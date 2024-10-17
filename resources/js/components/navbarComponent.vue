74cd8360bdba2864b8c5d35ba9086335f8f2771d
Ngurahh <143855469+Ngurahh@users.noreply.github.com>
2024-10-17 13:12:25 +0800

    update/beranda

--- resources/js/components/navbarComponent.vue
+++ resources/js/components/navbarComponent.vue
@@ -1,100 +1,104 @@
 <template>
-    <nav class="navbar navbar-expand-lg">
-        <div class="container">
-            <a class="navbar-brand" href="#"><strong>LPD | Desa Adat Kampial</strong></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <router-link class="nav-link" :to="{ name: 'beranda' }">Beranda</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" :to="{ name: 'galeri' }">Galeri</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" :to="{ name: 'layanan' }">Layanan</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" :to="{ name: 'profil' }">Profil</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" :to="{ name: 'kontak' }">Kontak</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" :to="{ name: 'formulirPinjaman' }">Formulir Pinjaman</router-link>
-                    </li>
-                </ul>
-                <router-link class="btn-login nav-link" :to="{ name: 'masuk' }">Masuk</router-link>
-            </div>
-        </div>
-    </nav>
+  <nav class="navbar navbar-expand-lg">
+      <div class="container">
+          <a class="navbar-brand" href="#"><strong>LPD | Desa Adat Kampial</strong></a>
+          <a class="navbar-brand" href="#"><strong>LPD | Desa Adat Kampial</strong></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <ul class="navbar-nav">
+                  <li class="nav-item">
+                      <router-link class="nav-link" :to="{ name: 'beranda' }">Beranda</router-link>
+                  </li>
+                  <li class="nav-item">
+                      <router-link class="nav-link" :to="{ name: 'galeri' }">Galeri</router-link>
+                  </li>
+                  <li class="nav-item">
+                      <router-link class="nav-link" :to="{ name: 'layanan' }">Layanan</router-link>
+                  </li>
+                  <li class="nav-item">
+                      <router-link class="nav-link" :to="{ name: 'profil' }">Profil</router-link>
+                  </li>
+                  <li class="nav-item">
+                      <router-link class="nav-link" :to="{ name: 'kontak' }">Kontak</router-link>
+                  </li>
+                  <li class="nav-item">
+                      <router-link class="nav-link" :to="{ name: 'formulirPinjaman' }">Formulir Pinjaman</router-link>
+                  </li>
+              </ul>
+              <router-link class="btn-login nav-link" :to="{ name: 'masuk' }">Masuk</router-link>
+          </div>
+      </div>
+  </nav>
 </template>
 
 <style scoped>
 nav {
-  position: fixed;
-  top: 0;
-  width: 100%;
-  background-color: whitesmoke;
-  z-index: 1000;
-  box-shadow: rgba(99, 99, 99, 0.2) 0px 0px 8px 2px;
+position: fixed;
+top: 0;
+width: 100%;
+background-color: white;
+background-color: whitesmoke;
+z-index: 1000;
+box-shadow: rgba(99, 99, 99, 0.2) 0px 0px 8px 2px;
 }
 
 .navbar-brand, .nav-link {
-  color: #444444;
-  border-bottom: 2px solid transparent;
-  transition: all 0.4s ease;
+color: #444444;
+border-bottom: 2px solid transparent;
+transition: all 0.4s ease;
 }
 
 .nav-item {
-  margin-right: 1rem;
+margin-right: 1rem;
 }
 
 .nav-link:hover {
-  border-bottom: 2px solid #444444;
+border-bottom: 2px solid #444444;
 }
 
+.nav-link.router-link-active:not(.btn-login) {
 .nav-link.router-link-active:no-button {
-  color: #22980e;
-  border-bottom: 2px solid #22980e;
+color: #22980e;
+border-bottom: 2px solid #22980e;
 }
 
 .navbar-toggler,
 .navbar-toggler:focus {
-  box-shadow: none;
-  border: none;
+box-shadow: none;
+border: none;
 }
 
 .navbar-toggler-icon {
-  background-image: url("data:image/svg+xml;charset=UTF8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3E%3Cpath stroke='%23444444' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E");
+background-image: url("data:image/svg+xml;charset=UTF8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3E%3Cpath stroke='%23444444' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E");
 }
 
 .navbar-toggler-icon:hover {
-  background-image: url("data:image/svg+xml;charset=UTF8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3E%3Cpath stroke='%2322980e' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E");
+background-image: url("data:image/svg+xml;charset=UTF8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3E%3Cpath stroke='%2322980e' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E");
 }
 
 .navbar-nav {
-  margin: 0 auto;
+margin: 0 auto;
 }
 
 .btn-login {
-  padding: 0.5rem 1.5rem;
-  border-radius: 100px;
-  background-color: #22980e;
-  color: white;
-  text-align: center;
+padding: 0.5rem 1.5rem;
+border-radius: 100px;
+background-color: #22980e;
+color: white;
+text-align: center;
 }
 
 .btn-login:hover {
-  background-color: #145809;
+background-color: #145809;
 }
 
 @media (max-width: 990px) {
-    .btn-login {
-        width: 100%;
-        margin: 1.5rem 0 1rem;
-    }
+  .btn-login {
+      width: 100%;
+      margin: 1.5rem 0 1rem;
+  }
+}
 }
 </style>
--- resources/js/pages/berandaPage.vue
+++ resources/js/pages/berandaPage.vue
@@ -20,10 +20,64 @@
             <span class="visually-hidden">Next</span>
         </button>
     </div>
+
+    <div class="container">
+        <div class="section-1">
+            <div class="row">
+                <div class="col {-6">
+                    <h2 class="greeting"><strong>Om Swastiastu</strong></h2>
+                    <p class="description">
+                        Selamat Datang di Website Lembaga Perkreditan Desa (LPD) Desa Adat Kampial. 
+                        Ngiring Sareng – Sareng Ngewangun Desa Mejalaran Antuk Nyarengin Ring LPD. 
+                        Bermula dari di adakannya Lomba Desa Adat tingkat kabupaten di Badung, 
+                        pada bulan September 2000 , Desa Adat Kampial salah satunya ikut sebagai 
+                        peserta dalam pecentokan tersebut yang merupakan ivent tahunan dari Kabupaten 
+                        Badung dalam rangka perayaan HUT Kabupaten Badung.
+                    </p>
+                </div>
+                <div class="col {-6">
+                    <iframe class="youtube" src="https://www.youtube.com/embed/XFspqbU84WE?si=E8RVTV6jDaMBTuj9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
     .carousel {
-        margin: 3rem 0 5rem 0;
+        margin: 3rem 0 0 0;
+    }
+
+    .section-1 {
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+        margin: 3rem 0;
+        padding: 2rem;
+        border-radius: 10px;
+    }
+
+    .greeting {
+        margin-bottom: 0.8rem;
+    }
+
+    .description {
+        margin: 0;
+        line-height: 1.8rem;
+        font-size: 17px;
+    }
+
+    .youtube {
+        width: 100%;
+        height: 100%;
+        border-radius: 5px;
+    }
+
+    @media (max-width: 990px) {
+        .description {
+            margin-bottom: 1.5rem;
+        }
+
+        .youtube {
+            margin: 3rem 0 5rem 0;
+        }
     }
 </style>
